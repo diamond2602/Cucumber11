2cf806edef5c6d1773e411c246ffc4f54f80ca22
diamond2602 <39782754+diamond2602@users.noreply.github.com>
2022-03-30 13:33:12 -0700

    Update testgithubamazon.java

--- src/test/java/pages/amazonpages/testgithubamazon.java
+++ src/test/java/pages/amazonpages/testgithubamazon.java
@@ -4,5 +4,6 @@ public class testgithubamazon {
     public static void main(String[] args) {
         System.out.println("this is test for github");
         System.out.println("this is second change");
+        System.out.println("this is change from github");
     }
 }
