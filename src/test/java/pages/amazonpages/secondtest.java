5f4ae992f16331b9cd3e2fcc9847e1da56ca5b92
diamond2602 <diamond2602@gmail.com>
2022-03-30 14:42:41 -0700

    Create testfilefromgithub

--- src/test/java/pages/amazonpages/secondtest.java
+++
@@ -1,7 +0,0 @@
-package pages.amazonpages;
-
-public class secondtest {
-    public static void main(String[] args) {
-        System.out.println("second test");
-    }
-}
--- src/test/java/pages/amazonpages/testgithub.java
+++
@@ -1,7 +0,0 @@
-package pages.amazonpages;
-
-public class testgithub {
-    public static void main(String[] args) {
-        System.out.println("test for github");
-    }
-}
