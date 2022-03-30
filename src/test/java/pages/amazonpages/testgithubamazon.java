ff1953f3d7a06cb8b7cbe61418d3fea0b744dff5
diamond2602 <diamond2602@gmail.com>
2022-03-30 13:32:03 -0700

    cucumberb11firstcommit

---
+++ src/test/java/pages/amazonpages/testgithubamazon.java
@@ -0,0 +1,8 @@
+package pages.amazonpages;
+
+public class testgithubamazon {
+    public static void main(String[] args) {
+        System.out.println("this is test for github");
+        System.out.println("this is second change");
+    }
+}
